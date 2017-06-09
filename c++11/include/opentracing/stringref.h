128b71ec65a8d239f431948358f578a0e533948a
Ryan Burn <ryan.burn@gmail.com>
2017-06-09 15:55:31 -0400

    Update style of StringRef class.

--- c++11/include/opentracing/stringref.h
+++ c++11/include/opentracing/stringref.h
@@ -15,6 +15,20 @@
 //
 // See the boost documentation for more background:
 // http://www.boost.org/doc/libs/master/libs/utility/doc/html/string_ref.html
+//
+// -----
+// Note:
+// -----
+// Although we have the ability to use wide string refs, there are side
+// effects in exposing an OpenTracing interface that works with narrow and wide
+// strings at the same time. Storage on the implementation will have a 'native'
+// format.
+//
+// Exposing references to that format avoid copies means clients would be
+// dependent on that format. If they're dependent on that detail and then switch
+// out the implementation to a different format, there would be lots of code
+// that breaks if it was expecting wstring and starts receiving string all of a
+// sudden. That design issue still needs to be addressed.
 
 #include <cstring>
 #include <ostream>
@@ -35,115 +49,70 @@ inline namespace OPENTRACING_VERSION_NAMESPACE {
 
 class StringRef {
  public:
-  StringRef();
   // Construct an empty StringRef
+  StringRef() noexcept : data_(0), length_(0) {}
 
-  template <size_t N>
-  StringRef(const char (&str)[N]);
   // Explicitly create string reference from a const character array
-
-  explicit StringRef(const char* str);
-  // Explicitly create string reference from const character pointer
-
-  StringRef(const std::basic_string<char>& str);
-  // Create constant string reference from pointer and length
-
-  StringRef(const char* str, size_t len);
-  // Create constant string reference from pointer and length
-
-  operator const char*() const;
-  // Implicit conversion to plain char *
-
   template <size_t N>
-  void reset(const char (&str)[N]);
+  StringRef(const char (&str)[N]) noexcept : data_(str), length_(N - 1) {}
+
+  // Explicitly create string reference from const character pointer
+  explicit StringRef(const char* str) noexcept
+      : data_(str), length_(std::strlen(str)) {}
+
+  // Create constant string reference from pointer and length
+  StringRef(const std::basic_string<char>& str) noexcept
+      : data_(str.c_str()), length_(str.length()) {}
+
+  // Create constant string reference from pointer and length
+  StringRef(const char* str, size_t len) noexcept : data_(str), length_(len) {}
+
+  // Disallow construction from non-const array
+  template <size_t N>
+  StringRef(char (&str)[N]) = delete;
+
+  // Implicit conversion to plain char *
+  operator const char*() const noexcept { return data_; }
+
   // Reset the string reference given a const character array
+  template <size_t N>
+  void reset(const char (&str)[N]) noexcept {
+    data_ = str;
+    length_ = N;
+  }
 
-  void reset(const char* str);
   // Reset this string ref to point at the supplied c-string
+  void reset(const char* str) noexcept {
+    data_ = str;
+    length_ = std::strlen(str);
+  }
 
-  void reset(const std::basic_string<char>& str);
   // Reset the string reference given a std::string
+  void reset(const std::basic_string<char>& str) noexcept {
+    data_ = str.data();
+    length_ = str.length();
+  }
 
-  void reset(const char* str, const size_t length);
   // Reset this string ref to point at the supplied 'str' of 'length' bytes.
+  void reset(const char* str, const size_t length) noexcept {
+    data_ = str;
+    length_ = length;
+  }
 
-  const char* data() const;
+  // Disallow reset from non-const array
+  template <size_t N>
+  void reset(char (&str)[N]) = delete;
+
   // Return address of the referenced string
+  const char* data() const noexcept { return data_; }
 
-  size_t length() const;
   // Return the length of the referenced string
+  size_t length() const noexcept { return length_; }
 
  private:
-  template <size_t N>
-  StringRef(char (&str)[N]);
-  // Disallow construction from non-const array
-
-  template <size_t N>
-  void reset(char (&str)[N]);
-  // Disallow reset from non-const array
-
-  const char* m_data;  // Pointer to external storage
-  size_t m_length;     // Length of data pointed to by 'm_data'
+  const char* data_;  // Pointer to external storage
+  size_t length_;     // Length of data pointed to by 'data_'
 };
-
-// -----
-// Note:
-// -----
-// Although we have the ability to use wide string refs, there are side
-// effects in exposing an OpenTracing interface that works with narrow and wide
-// strings at the same time. Storage on the implementation will have a 'native'
-// format.
-//
-// Exposing references to that format avoid copies means clients would be
-// dependent on that format. If they're dependent on that detail and then switch
-// out the implementation to a different format, there would be lots of code
-// that breaks if it was expecting wstring and starts receiving string all of a
-// sudden. That design issue still needs to be addressed.
-
-// ---------------
-// Class StringRef
-// ---------------
-
-inline StringRef::StringRef() : m_data(0), m_length(0) {}
-
-template <size_t N>
-StringRef::StringRef(const char (&str)[N]) : m_data(str), m_length(N - 1) {}
-
-inline StringRef::StringRef(const char* str)
-    : m_data(str), m_length(std::strlen(str)) {}
-
-inline StringRef::StringRef(const std::basic_string<char>& str)
-    : m_data(str.c_str()), m_length(str.length()) {}
-
-inline StringRef::StringRef(const char* str, size_t len)
-    : m_data(str), m_length(len) {}
-
-inline StringRef::operator const char*() const { return m_data; }
-
-inline void StringRef::reset(const char* str, const size_t length) {
-  m_data = str;
-  m_length = length;
-}
-
-template <size_t N>
-void StringRef::reset(const char (&str)[N]) {
-  m_data = str;
-  m_length = N;
-}
-
-inline void StringRef::reset(const char* str) {
-  m_data = str;
-  m_length = std::strlen(str);
-}
-
-inline void StringRef::reset(const std::basic_string<char>& str) {
-  m_data = str.data();
-  m_length = str.length();
-}
-
-inline const char* StringRef::data() const { return m_data; }
-
-inline size_t StringRef::length() const { return m_length; }
 
 inline std::ostream& operator<<(std::ostream& os,
                                 const opentracing::StringRef& ref) {
