4df0a78cfc45ce2bb3c60cf17552a1eecde61131
Ryan Burn <ryan.burn@gmail.com>
2017-06-16 19:49:43 -0400

    Correct clang-tidy warnings.

--- c++11/src/noop.cpp
+++ c++11/src/noop.cpp
@@ -6,8 +6,8 @@ namespace {
 class NoopSpanContext : public SpanContext {
  public:
   void ForeachBaggageItem(
-      std::function<bool(const std::string& key, const std::string& value)> f)
-      const override {}
+      std::function<bool(const std::string& key,
+                         const std::string& value)> /*f*/) const override {}
 };
 
 class NoopSpan : public Span {
@@ -15,16 +15,17 @@ class NoopSpan : public Span {
   explicit NoopSpan(std::shared_ptr<const Tracer>&& tracer) noexcept
       : tracer_(std::move(tracer)) {}
   void FinishWithOptions(
-      const FinishSpanOptions& finish_span_options) noexcept override {}
-  void SetOperationName(StringRef name) noexcept override {}
-  void SetTag(StringRef key, const Value& value) noexcept override {}
-  void SetBaggageItem(StringRef restricted_key,
-                      StringRef value) noexcept override {}
-  std::string BaggageItem(StringRef restricted_key) const noexcept override {
+      const FinishSpanOptions& /*finish_span_options*/) noexcept override {}
+  void SetOperationName(StringRef /*name*/) noexcept override {}
+  void SetTag(StringRef /*key*/, const Value& /*value*/) noexcept override {}
+  void SetBaggageItem(StringRef /*restricted_key*/,
+                      StringRef /*value*/) noexcept override {}
+  std::string BaggageItem(StringRef /*restricted_key*/) const
+      noexcept override {
     return {};
   }
   void Log(std::initializer_list<std::pair<StringRef, Value>>
-               fields) noexcept override {}
+           /*fields*/) noexcept override {}
   const SpanContext& context() const noexcept override { return span_context_; }
   const Tracer& tracer() const noexcept override { return *tracer_; }
 
@@ -37,19 +38,20 @@ class NoopTracer : public Tracer,
                    public std::enable_shared_from_this<NoopTracer> {
  public:
   std::unique_ptr<Span> StartSpanWithOptions(
-      StringRef operation_name, const StartSpanOptions& options) const
+      StringRef /*operation_name*/, const StartSpanOptions& /*options*/) const
       noexcept override {
     return std::unique_ptr<Span>(new (std::nothrow)
                                      NoopSpan(shared_from_this()));
   }
 
-  Expected<void> Inject(const SpanContext& sc, CarrierFormat format,
-                        const CarrierWriter& writer) const override {
+  Expected<void> Inject(const SpanContext& /*sc*/, CarrierFormat /*format*/,
+                        const CarrierWriter& /*writer*/) const override {
     return {};
   }
 
   Expected<std::unique_ptr<SpanContext>> Extract(
-      CarrierFormat format, const CarrierReader& reader) const override {
+      CarrierFormat /*format*/,
+      const CarrierReader& /*reader*/) const override {
     return std::unique_ptr<SpanContext>(nullptr);
   }
 };
--- c++11/src/propagation.cpp
+++ c++11/src/propagation.cpp
@@ -11,29 +11,35 @@ class PropagationErrorCategory : public std::error_category {
 
   std::error_condition default_error_condition(int code) const
       noexcept override {
-    if (code == unsupported_format_error.value())
+    if (code == unsupported_format_error.value()) {
       return std::make_error_condition(std::errc::not_supported);
-    else if (code == invalid_span_context_error.value())
+    }
+    if (code == invalid_span_context_error.value()) {
       return std::make_error_condition(std::errc::not_supported);
-    else if (code == invalid_carrier_error.value())
+    }
+    if (code == invalid_carrier_error.value()) {
       return std::make_error_condition(std::errc::invalid_argument);
-    else if (code == span_context_corrupted_error.value())
+    }
+    if (code == span_context_corrupted_error.value()) {
       return std::make_error_condition(std::errc::invalid_argument);
-    else
-      return std::error_condition(code, *this);
+    }
+    return std::error_condition(code, *this);
   }
 
   std::string message(int code) const override {
-    if (code == unsupported_format_error.value())
+    if (code == unsupported_format_error.value()) {
       return "opentracing: Unknown or unsupported Inject/Extract format";
-    else if (code == invalid_span_context_error.value())
+    }
+    if (code == invalid_span_context_error.value()) {
       return "opentracing: SpanContext type incompatible with tracer";
-    else if (code == invalid_carrier_error.value())
+    }
+    if (code == invalid_carrier_error.value()) {
       return "opentracing: Invalid Inject/Extract carrier";
-    else if (code == span_context_corrupted_error.value())
+    }
+    if (code == span_context_corrupted_error.value()) {
       return "opentracing: SpanContext data corrupted in Extract carrier";
-    else
-      return "opentracing: unknown propagation error";
+    }
+    return "opentracing: unknown propagation error";
   }
 };
 }  // anonymous namespace
