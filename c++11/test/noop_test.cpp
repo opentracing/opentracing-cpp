1346d0a4db63528a146dfe6b7560f4c08d027bc3
Ryan Burn <ryan.burn@gmail.com>
2017-06-17 12:19:34 -0400

    Check for null in noop_test.

--- c++11/test/noop_test.cpp
+++ c++11/test/noop_test.cpp
@@ -7,9 +7,11 @@ int main() {
   auto tracer = make_noop_tracer();
 
   auto span1 = tracer->StartSpan("a");
+  assert(span1);
   assert(&span1->tracer() == tracer.get());
 
   auto span2 = tracer->StartSpan("b", {ChildOf(&span1->context())});
+  assert(span2);
   span2->SetOperationName("b1");
   span2->SetTag("x", true);
   assert(span2->BaggageItem("y").empty());
