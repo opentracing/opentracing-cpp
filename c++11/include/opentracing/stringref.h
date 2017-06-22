681eff85398a7733778416ff154f573f783ed5bd
Ryan Burn <ryan.burn@gmail.com>
2017-06-22 19:01:49 -0400

    Changed StringRef's API to more closely match std::string_view.

--- c++11/include/opentracing/stringref.h
+++ c++11/include/opentracing/stringref.h
@@ -54,13 +54,8 @@ class StringRef {
   // Construct an empty StringRef
   StringRef() noexcept : data_(nullptr), length_(0) {}
 
-  // Explicitly create string reference from a const character array
-  template <size_t N>
-  StringRef(const char (&str)[N]) noexcept : data_(str), length_(N - 1) {}
-
-  // Explicitly create string reference from const character pointer
-  explicit StringRef(const char* str) noexcept
-      : data_(str), length_(std::strlen(str)) {}
+  // create string reference from const character pointer
+  StringRef(const char* str) noexcept : data_(str), length_(std::strlen(str)) {}
 
   // Create constant string reference from pointer and length
   StringRef(const std::basic_string<char>& str) noexcept
@@ -69,47 +64,18 @@ class StringRef {
   // Create constant string reference from pointer and length
   StringRef(const char* str, size_t len) noexcept : data_(str), length_(len) {}
 
-  // Disallow construction from non-const array
-  template <size_t N>
-  StringRef(char (&str)[N]) = delete;
-
   // Implicit conversion to std::string
   operator std::string() const { return {data_, length_}; }
-
-  // Reset the string reference given a const character array
-  template <size_t N>
-  void Reset(const char (&str)[N]) noexcept {
-    data_ = str;
-    length_ = N;
-  }
-
-  // Reset this string ref to point at the supplied c-string
-  void Reset(const char* str) noexcept {
-    data_ = str;
-    length_ = std::strlen(str);
-  }
-
-  // Reset the string reference given a std::string
-  void Reset(const std::basic_string<char>& str) noexcept {
-    data_ = str.data();
-    length_ = str.length();
-  }
-
-  // Reset this string ref to point at the supplied 'str' of 'length' bytes.
-  void Reset(const char* str, const size_t length) noexcept {
-    data_ = str;
-    length_ = length;
-  }
-
-  // Disallow Reset from non-const array
-  template <size_t N>
-  void Reset(char (&str)[N]) = delete;
 
   // Return address of the referenced string
   const char* data() const noexcept { return data_; }
 
+  // Returns true if `length_` == 0
+  bool empty() const { return length_ == 0; }
+
   // Return the length of the referenced string
   size_t length() const noexcept { return length_; }
+  size_t size() const noexcept { return length_; }
 
   // Returns a RandomAccessIterator to the first element.
   const char* begin() const noexcept { return data(); }
--- c++11/test/stringref_test.cpp
+++ c++11/test/stringref_test.cpp
@@ -19,15 +19,6 @@ static void test_cstring() {
   assert(std::strlen(val) == ref.length());
 }
 
-static void test_cstring_array() {
-  const char val[] = "hello world";
-
-  StringRef ref(val);
-
-  assert(val == ref.data());
-  assert(std::strlen(val) == ref.length());
-}
-
 static void test_std_string() {
   const std::string val = "hello world";
 
@@ -47,54 +38,10 @@ static void test_copy() {
   assert(val.length() == cpy.length());
 }
 
-static void test_reset() {
-  StringRef ref;
-  assert(0 == ref.data());
-  assert(0 == ref.length());
-
-  ref.Reset("hello world");
-
-  assert("hello world" == ref);
-  assert(std::string("hello world") == std::string(ref.data()));
-  assert(std::strlen("hello world") == ref.length());
-
-  const char arr[] = "hello world 1";
-
-  ref.Reset(arr);
-  assert(arr == ref.data());
-  assert(std::strlen(arr) == ref.length());
-  assert(std::string(arr) == std::string(ref));
-  assert(std::string(arr) == std::string(ref.data()));
-
-  const char* p = "hello world 2";
-
-  ref.Reset(p);
-  assert(p == ref.data());
-  assert(std::strlen(p) == ref.length());
-  assert(std::string(p) == std::string(ref));
-  assert(std::string(p) == std::string(ref.data()));
-
-  const std::string s = "hello world 3";
-
-  ref.Reset(s);
-  assert(s.data() == ref.data());
-  assert(s.length() == ref.length());
-  assert(std::string(s.c_str()) == std::string(ref));
-  assert(std::string(s.c_str()) == std::string(ref.data()));
-
-  ref.Reset(p, std::strlen(p));
-  assert(p == ref.data());
-  assert(std::strlen(p) == ref.length());
-  assert(p == ref);
-  assert(std::string(p) == std::string(ref.data()));
-}
-
 int main() {
   test_empty();
   test_cstring();
-  test_cstring_array();
   test_std_string();
   test_copy();
-  test_reset();
   return 0;
 }
