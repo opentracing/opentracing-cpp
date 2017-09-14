4916baea34b424aee8a644454eda9f7973130b63
Stefan Pantos <stefan.pantos@torstonetech.com>
2017-09-14 08:03:27 +0100

    Correct the documentation so that the static target matches.

--- c++11/cmake/OpenTracingConfig.cmake
+++ c++11/cmake/OpenTracingConfig.cmake
@@ -4,7 +4,7 @@
 #    find_package(OpenTracing 0.1.0)  # To specify a minimum version
 # This will create the targets:
 #    OpenTracing::opentracing # dynamic library
-#    OpenTracing::opentracing_static # static library
+#    OpenTracing::opentracing-static # static library
 #
 # Linking to these libraries will also add the approprate interface header path to your compilation line.
 # e.g. target_link_libraries(newtarget OpenTracing::opentracing)
