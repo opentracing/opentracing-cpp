a23f08e9da037a0389d23fbf06d27c0e7351bf05
Isaac Hier <isaachier@gmail.com>
2017-10-01 08:11:40 -0400

    More fixes.

--- src/propagation.cpp
+++ src/propagation.cpp
@@ -8,7 +8,9 @@ class PropagationErrorCategory : public std::error_category {
   // Needed to fix bug in macOS build
   // (https://travis-ci.org/isaachier/hunter/jobs/281868518).
   // See https://stackoverflow.com/a/7411708/1930331 for justification.
-  PropagationErrorCategory() = default;
+  PropagationErrorCategory()
+  {
+  }
 
   const char* name() const noexcept override {
     return "OpenTracingPropagationError";
