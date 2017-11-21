f026cf3ef97a5165804115b9f7b14e5670e8da98
Ryan Burn <ryan.burn@gmail.com>
2017-11-20 18:38:53 -0800

    Move comments for InMemoryStream.

--- src/in_memory_stream.h
+++ src/in_memory_stream.h
@@ -6,13 +6,13 @@
 
 namespace opentracing {
 BEGIN_OPENTRACING_ABI_NAMESPACE
-// Class to allow construction of an istream from constant memory without
-// copying. See https://stackoverflow.com/a/13059195/4447365.
 class InMemoryBuffer : public std::streambuf {
  public:
   InMemoryBuffer(const char* data, size_t size);
 };
 
+// Class to allow construction of an istream from constant memory without
+// copying. See https://stackoverflow.com/a/13059195/4447365.
 class InMemoryStream : virtual public InMemoryBuffer, public std::istream {
  public:
   InMemoryStream(const char* data, size_t size);
