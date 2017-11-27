31bf49421958bdbd190c72cdcd9910d262d9df21
Ryan Burn <ryan.burn@gmail.com>
2017-11-27 12:05:31 -0800

    Use custom assert.

--- test/tracer_test.cpp
+++ test/tracer_test.cpp
@@ -1,23 +1,21 @@
-// Make sure assert is defined.
-#undef NDEBUG
+#include "assert.h"
 
 #include <opentracing/noop.h>
 #include <opentracing/tracer.h>
-#include <cassert>
 using namespace opentracing;
 
 static void test_tracer_interface() {
   auto tracer = MakeNoopTracer();
 
   auto span1 = tracer->StartSpan("a");
-  assert(span1);
-  assert(&span1->tracer() == tracer.get());
+  ASSERT(span1);
+  ASSERT(&span1->tracer() == tracer.get());
 
   auto span2 = tracer->StartSpan("b", {ChildOf(&span1->context())});
-  assert(span2);
+  ASSERT(span2);
   span2->SetOperationName("b1");
   span2->SetTag("x", true);
-  assert(span2->BaggageItem("y").empty());
+  ASSERT(span2->BaggageItem("y").empty());
   span2->Log({{"event", "xyz"}, {"abc", 123}});
   span2->Finish();
 }
@@ -27,7 +25,7 @@ static void test_start_span_options() {
 
   // A reference to null a SpanContext is ignored.
   ChildOf(nullptr).Apply(options);
-  assert(options.references.size() == 0);
+  ASSERT(options.references.size() == 0);
 }
 
 int main() {
