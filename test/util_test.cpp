b09a36a57d81accee207eda20f0acfd0f125990b
Ryan Burn <ryan.burn@gmail.com>
2018-01-04 20:56:43 +0000

    Convert util_test to use catch2.

--- test/util_test.cpp
+++ test/util_test.cpp
@@ -1,11 +1,12 @@
-#include "assert.h"
-
 #include <opentracing/util.h>
 #include <cmath>
 #include <cstdlib>  // Work around to https://stackoverflow.com/a/30084734.
 using namespace opentracing;
 
-static void test_convert_time_point() {
+#define CATCH_CONFIG_MAIN
+#include <opentracing/catch2/catch.hpp>
+
+TEST_CASE("convert_time_point") {
   // Check that converting from a system_clock time_point to a steady_clock
   // time point and then back again produces approximately the same system_clock
   // time_point.
@@ -14,10 +15,5 @@ static void test_convert_time_point() {
   auto t3 = convert_time_point<SystemClock>(t2);
   auto difference = std::abs(
       std::chrono::duration_cast<std::chrono::microseconds>(t3 - t1).count());
-  ASSERT(difference < 100);
-}
-
-int main() {
-  test_convert_time_point();
-  return 0;
+  CHECK(difference < 100);
 }
