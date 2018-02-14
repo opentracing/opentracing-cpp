6796a85b6a8e06449a4a644ad9153dffafe37f1a
Ryan Burn <ryan.burn@gmail.com>
2018-02-14 13:34:38 -0500

    Remove constexpr from rvalue-ref member.

--- 3rd_party/include/opentracing/expected/expected.hpp
+++ 3rd_party/include/opentracing/expected/expected.hpp
@@ -694,7 +694,7 @@ public:
             : ( error_traits<error_type>::rethrow( contained.error() ), contained.value() );
     }
 
-    constexpr value_type && value() &&
+    value_type && value() &&
     {
         return has_value()
             ? ( contained.value() )
