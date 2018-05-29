f435d2b8b2ac358e4e7b21031c4073968627a9c4
Ryan Burn <ryan.burn@gmail.com>
2018-05-29 14:04:19 -0400

    Fix permissions.

--- ci/setup_linux_environment.sh
+++ ci/setup_linux_environment.sh
@@ -7,9 +7,9 @@ sudo apt-get install --no-install-recommends --no-install-suggests -y \
          curl \
          ca-certificates \
          openjdk-8-jdk
-sudo echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" \
-      | tee /etc/apt/sources.list.d/bazel.list
-sudo curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" \
+      | sudo tee /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install --no-install-recommends --no-install-suggests -y \
          bazel
