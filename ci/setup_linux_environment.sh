7bae64a2e4204753d34f82e7df6f718dfdbe6cad
Ryan Burn <ryan.burn@gmail.com>
2018-05-29 13:56:08 -0400

    Fix travis maybe.

--- ci/setup_linux_environment.sh
+++ ci/setup_linux_environment.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-sudo apt-get update 
-sudo apt-get install --no-install-recommends --no-install-suggests -y \
+apt-get update 
+apt-get install --no-install-recommends --no-install-suggests -y \
          curl \
          ca-certificates \
          openjdk-8-jdk
-sudo echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" \
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" \
       | tee /etc/apt/sources.list.d/bazel.list
-sudo curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
-sudo apt-get update
-sudo apt-get install --no-install-recommends --no-install-suggests -y \
+curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
+apt-get update
+apt-get install --no-install-recommends --no-install-suggests -y \
          bazel
-sudo apt-get upgrade -y bazel
+apt-get upgrade -y bazel
