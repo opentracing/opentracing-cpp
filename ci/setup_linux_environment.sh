4997e467c27f2ee26f5a6f04d51ce2e93aee172a
Ryan <ryan.burn@gmail.com>
2018-05-29 14:57:07 -0400

    Set up bazel CI. (#90)
    * Set up bazel CI.
    
    * Fix travis config.
    
    * Fix permissions.
    
    * Update homebrew before installing bazel.
    
    * Fix travis maybe.
    
    * Fix travis maybe.
    
    * Fix permissions.
    
    * Try faster install instructions.
    
    * Fix bazel linker error.

---
+++ ci/setup_linux_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl --silent https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/bazel.list" \
+    -o Dir::Etc::sourceparts="-" \
+    -o APT::Get::List-Cleanup="0"
+sudo apt-get install openjdk-8-jdk bazel
