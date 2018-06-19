4d3bf1af528d3eec586384e3a8dcd39cf21a355a
Ryan Burn <ryan.burn@gmail.com>
2018-06-19 13:45:26 -0400

    Make sure error_message is set when dynamic loading.

--- src/dynamic_load_unix.cpp
+++ src/dynamic_load_unix.cpp
@@ -65,13 +65,16 @@ DynamicallyLoadTracingLibrary(const char* shared_library,
       OPENTRACING_VERSION, OPENTRACING_ABI_VERSION, &error_category,
       static_cast<void*>(&error_message), &tracer_factory);
   if (rcode != 0) {
-    if (error_category != nullptr) {
-      return make_unexpected(std::error_code{
-          rcode, *static_cast<const std::error_category*>(error_category)});
-    } else {
+    if (error_category == nullptr) {
       error_message = "failed to construct a TracerFactory: unknown error code";
       return make_unexpected(dynamic_load_failure_error);
     }
+    const auto error_code = std::error_code{
+        rcode, *static_cast<const std::error_category*>(error_category)};
+    if (error_message.empty()) {
+      error_message = error_code.message();
+    }
+    return make_unexpected(error_code);
   }
 
   if (tracer_factory == nullptr) {
