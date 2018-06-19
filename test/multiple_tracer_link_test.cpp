e1cc1b66c36a91b8f57212ce7889f644264c63db
Ryan Burn <ryan.burn@gmail.com>
2018-06-19 16:25:33 -0400

    Make sure OpenTracingMakeTracerFactory isn't elided in tests.

--- test/multiple_tracer_link_test.cpp
+++ test/multiple_tracer_link_test.cpp
@@ -1,3 +1,14 @@
-// Contains nothing, but links in tracer_a.o and tracer_b.o to verify that
-// there's no multiple definition error from OpenTracingMakeTracerFactory.
-int main() { return 0; }
+// Links in tracer_a.o and tracer_b.o to verify that there's no multiple
+// definition error from OpenTracingMakeTracerFactory.
+#include <opentracing/dynamic_load.h>
+
+extern OpenTracingMakeTracerFactoryType* const OpenTracingMakeTracerFactory;
+
+int main() {
+  // Call OpenTracingMakeTracerFactory to make sure it's not elided.
+  if ((*OpenTracingMakeTracerFactory)(nullptr, nullptr, nullptr, nullptr,
+                                      nullptr) != -1) {
+    return -1;
+  }
+  return 0;
+}
