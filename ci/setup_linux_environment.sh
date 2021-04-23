f86b33f3d9e7322b1298ba62d5ffa7a9519c4c41
Krishna Harsha Voora <14876995+krishvoor@users.noreply.github.com>
2021-04-23 23:07:20 +0530

    Enable testing on ppc64le architecture (#136)
    * Enable opentracing-cpp on ppc64le 
    
    This PR enables opentracing-cpp on ppc64le architecture
    and CI/CD build and tests ( if any ) are triggered via travis.yml
    
    Signed-off-by: Krishna Harsha Voora <krishvoor@in.ibm.com>
    
    * Enable ppc64le architecture in opentracing-cpp
    
    Signed-off-by: Krishna Harsha Voora <krishvoor@in.ibm.com>
    
    * Updates Distribution to Xenial
    
    Signed-off-by: Krishna Harsha Voora <krishvoor@in.ibm.com>
    
    * Minor Nit
    
    Signed-off-by: Krishna Harsha Voora <krishvoor@in.ibm.com>
    
    * Updates ci/setup_linux_environment.sh
    
    This PR adds required privileges to install packages
    on ppc64le
    
    Signed-off-by: Krishna Harsha Voora <krishvoor@in.ibm.com>
    
    * Update macOS Version 
    
    This PR Updates macOS Version in the travis file

--- ci/setup_linux_environment.sh
+++ ci/setup_linux_environment.sh
@@ -2,14 +2,27 @@
 
 set -e
 
+ARCH=`arch`
+
+if [[ "$ARCH" = "ppc64le" ]]; then
+sudo apt install gnupg2 apt-transport-https -y
+echo "deb https://oplab9.parqtec.unicamp.br/pub/repository/debian/ ./" | sudo tee /etc/apt/sources.list.d/bazel_ppc64le.list
+curl --silent  https://oplab9.parqtec.unicamp.br/pub/key/openpower-gpgkey-public.asc  -o /tmp/openpower-gpgkey-public.asc
+sudo apt-key add /tmp/openpower-gpgkey-public.asc
+sudo rm -f /tmp/openpower-gpgkey-public.asc 
+sudo apt update	
+
+else
+
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 curl --silent https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
 sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/bazel.list" \
-    -o Dir::Etc::sourceparts="-" \
-    -o APT::Get::List-Cleanup="0"
-sudo apt-get install openjdk-8-jdk bazel
-sudo apt-get install software-properties-common
+-o Dir::Etc::sourceparts="-" \
+-o APT::Get::List-Cleanup="0"
 
-sudo add-apt-repository ppa:george-edison55/cmake-3.x -y
+fi
+
 sudo apt-get update
-sudo apt-get install cmake
+sudo apt-get install openjdk-8-jdk bazel -y
+sudo apt-get install software-properties-common -y
+sudo apt-get install cmake -y
